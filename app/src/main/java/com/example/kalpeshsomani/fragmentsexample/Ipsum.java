afcd09d3b706f765426a8fac39efaca967a8dc14
kalpesh somani <kalpeshsomani30@gmail.com>
2017-10-03 23:36:07 +0530

    Initial commit

---
+++ app/src/main/java/com/example/kalpeshsomani/fragmentsexample/Ipsum.java
@@ -0,0 +1,19 @@
+package com.example.kalpeshsomani.fragmentsexample;
+
+/**
+ * Created by kalpesh somani on 10/3/2017.
+ */
+
+public class Ipsum {
+
+
+    static String[] Headlines = {
+            "Article One",
+            "Article Two"
+    };
+
+    static String[] Articles = {
+            "Article One\n\nWhen life has got you in a slump, turn to these inspirational short stories. Not only is reading them like getting an internet hug for the soul, but they just may spark an idea or a change in you for the better. Read on and get ready to smile.",
+            "Article Two\n\nEvery single person on the planet has a story. Don’t judge people before you truly know them. The truth might surprise you."
+    };
+}
